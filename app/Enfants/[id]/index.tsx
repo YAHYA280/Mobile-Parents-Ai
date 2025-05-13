37b580345025fe78d0735ff57bfd0693e8215a82
yahyaELMOKHTARI <elmokhtariyahya2001@gmail.com>
2025-05-13 10:59:21 +0100

    Deleted the whole fucking section :D

--- app/Enfants/[id]/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Redirect, useLocalSearchParams } from "expo-router";
-
-export default function ChildDefaultScreen() {
-  const { id } = useLocalSearchParams();
-  // Use the actual ID value, not a placeholder
-  return <Redirect href={`/Enfants/${id}/apercu`} />;
-}
